6aad8b5635c1194997b505f51190a9864e4320a2
KaarthiV88 <39105708+KaarthiV88@users.noreply.github.com>
2023-06-25 11:49:42 -0500

    create Bird Class to flappy_bird.py

--- flappy_bird.py
+++ flappy_bird.py
@@ -12,3 +12,28 @@ PIPE_IMG = pygame.transform.scale2x(pygame.image.load(os.pag.join("FB_Imgs", "FB
 GROUND_IMG = pygame.transform.scale2x(pygame.image.load(os.pag.join("FB_Imgs", "FBground.png")))
 BG_IMG = pygame.transform.scale2x(pygame.image.load(os.pag.join("FB_Imgs", "FBbg.png")))
 
+class Bird:
+    IMGS = BIRD_IMGS
+    maxRotation = 25
+    rotationVelocity = 20
+    aniTime = 3.5
+    
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.tilt = 0
+        self.tickCount = 0
+        self.velocity = 0
+        self.height = self.y
+        self.imgCount = 0
+        self.img = self.IMGS[0]
+        
+    def jump(self):
+        self.velocity = -10
+        self.tickCount = 0
+        self.height = self.y
+        
+    def move(self):
+        self.tickCount += 1
+        displacement = self.velocity * self.tickCount + 0.5*3*self.tickCount**2
+
