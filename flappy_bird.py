0239222e687a372f95bf90b53fd006ae59a94038
KaarthiV88 <39105708+KaarthiV88@users.noreply.github.com>
2023-06-25 11:41:12 -0500

    initialize the FB images to flappy_bird.py

--- flappy_bird.py
+++ flappy_bird.py
@@ -7,5 +7,8 @@ import random
 windowWidth = 600
 windowHeight = 800
 
-
+BIRD_IMGS = [pygame.transform.scale2x(pygame.image.load(os.pag.join("FB_Imgs", "bird1.png"))),pygame.transform.scale2x(pygame.image.load(os.pag.join("FB_Imgs", "bird2.png"))), pygame.transform.scale2x(pygame.image.load(os.pag.join("FB_Imgs", "bird3.png")))] 
+PIPE_IMG = pygame.transform.scale2x(pygame.image.load(os.pag.join("FB_Imgs", "FBpipe.png")))
+GROUND_IMG = pygame.transform.scale2x(pygame.image.load(os.pag.join("FB_Imgs", "FBground.png")))
+BG_IMG = pygame.transform.scale2x(pygame.image.load(os.pag.join("FB_Imgs", "FBbg.png")))
 
