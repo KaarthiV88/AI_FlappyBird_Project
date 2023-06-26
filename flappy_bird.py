fea7e15a468f02e09319c968c804b999c0b3f7f2
KaarthiV88 <39105708+KaarthiV88@users.noreply.github.com>
2023-06-25 19:11:37 -0500

    Add 'main()' and run basic window flappy_bird.py

--- flappy_bird.py
+++ flappy_bird.py
@@ -3,20 +3,23 @@ import neat
 import time 
 import os 
 import random
+import math
+import numpy as np
 
-windowWidth = 600
-windowHeight = 800
+windowWidth = 800
+windowHeight = 1000
 
-BIRD_IMGS = [pygame.transform.scale2x(pygame.image.load(os.pag.join("FB_Imgs", "bird1.png"))),pygame.transform.scale2x(pygame.image.load(os.pag.join("FB_Imgs", "bird2.png"))), pygame.transform.scale2x(pygame.image.load(os.pag.join("FB_Imgs", "bird3.png")))] 
-PIPE_IMG = pygame.transform.scale2x(pygame.image.load(os.pag.join("FB_Imgs", "FBpipe.png")))
-GROUND_IMG = pygame.transform.scale2x(pygame.image.load(os.pag.join("FB_Imgs", "FBground.png")))
-BG_IMG = pygame.transform.scale2x(pygame.image.load(os.pag.join("FB_Imgs", "FBbg.png")))
+
+BIRD_IMGS = [pygame.transform.scale2x(pygame.image.load(os.path.join("FB_Imgs", "bird1.png"))),pygame.transform.scale2x(pygame.image.load(os.path.join("FB_Imgs", "bird2.png"))), pygame.transform.scale2x(pygame.image.load(os.path.join("FB_Imgs", "bird3.png")))] 
+PIPE_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("FB_Imgs", "FBpipe.png")))
+GROUND_IMG = (pygame.image.load(os.path.join("FB_Imgs", "FBground.png")))
+BG_IMG = (pygame.image.load(os.path.join("FB_Imgs", "FBbg.png")))
 
 class Bird:
     IMGS = BIRD_IMGS
     maxRotation = 25
     rotationVelocity = 20
-    aniTime = 3.5
+    aniTime = 5
     
     def __init__(self, x, y):
         self.x = x
@@ -40,11 +43,11 @@ class Bird:
         if(displacement >= 16):
             displacement = 16
             
-        if(d < 0):
-            d -= 1.75
+        if(displacement < 0):
+            displacement -= 1.75
             
         self.y += displacement
-        if(d<0 or self.y < (self.height+50)):
+        if(displacement<0 or self.y < (self.height+50)):
             if(self.tilt < self.maxRotation):
                 self.tilt = self.maxRotation
                 
@@ -67,5 +70,41 @@ class Bird:
             self.img = self.IMGS[0]
             self.imgCount = 0
             
+        
+        if(self.tilt <= -85):
+            self.img = self.IMGS[1]
+            self.imgCount = self.aniTime*2
             
+        rotatedImages = pygame.transform.rotate(self.img, self.tilt)
+        newRectangle = rotatedImages.get_rect(center = self.img.get_rect(topleft = (self.x, self.y)).center)
+        win.blit(rotatedImages, newRectangle.topleft)
+        
+        #blitRotateCenter(win, self.img, (self.x, self.y), self.tilt)
+    
+    def get_mask(self):
+        return pygame.mask.from_surface(self.img)
+    
+    
 
+
+def draw_window(win, bird): 
+    win.blit(BG_IMG, (-10,-10))
+    bird.draw(win)
+    pygame.display.update()
+    
+def main():
+    bird = Bird(200, 200)
+    window = pygame.display.set_mode((windowWidth, windowHeight))
+    run = True
+    
+    while(run == True):
+        for event in pygame.event.get():
+            if(event.type == pygame.QUIT):
+                run = False
+        
+        draw_window(window, bird)
+    
+    pygame.quit()
+    quit()
+    
+main()
